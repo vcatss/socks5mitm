f027b26d40e1fd3666c1331c0c5fbe7d5f1c31ec
vcatss <93695419+vcatss@users.noreply.github.com>
2022-08-31 11:23:31 +0700

    Update simple_logger.py

--- simple_logger.py
+++ simple_logger.py
@@ -1,18 +1,22 @@
 import sys
 from socks5mitm.server import SOCKS5handler, start_server
 from socks5mitm.server import exchange_loop, create_socket
-
+import pprint
+from socks5mitm import protocol, proxy
 
 PORT = int(sys.argv[1]) if len(sys.argv) > 1 else 4444
-
 
 class Handle(SOCKS5handler):
     def handle(self):
         self.handle_handshake()
         address = self.handle_address()
-        print(f"Request to {address[0]}:{address[1]}")
-        exchange_loop(self.request, create_socket(*address), self)
-
+        skt = proxy.socks5(("",1000),address)
+        #print(f"Request to {address[0]}:{address[1]}")
+        if 1:
+            skt = proxy.socks5(("127.0.0.1", 9050), address)
+        else:
+            skt = create_socket(*address)
+        exchange_loop(self.request, skt, self)
 
 print(f"Starting 127.0.0.1:{PORT}...")
 start_server(Handle, port=PORT)
