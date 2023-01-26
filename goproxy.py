64c10dfe2cf5ef1f2d108eb65e7c9d3b620903e5
VC <namnguyen.vc@gmail.com>
2023-01-26 16:06:30 +0700

    zz

--- goproxy.py
+++ goproxy.py
@@ -1,8 +1,8 @@
 import subprocess
 import threading
 
-def execute_command():
-    process = subprocess.Popen(["proxy", "socks", "-t", "tcp", "-p", "0.0.0.0:4444", "-P", "171.232.94.200:32883"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+def execute_command(ip):
+    process = subprocess.Popen(["proxy", "socks", "-t", "tcp", "-p", "0.0.0.0:4444", "-P", ip], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     while True:
         output = process.stdout.readline()
         if output == '' and process.poll() is not None:
@@ -11,7 +11,7 @@ def execute_command():
             print(output.strip())
 
 def read_output():
-    thread = threading.Thread(target=execute_command)
+    thread = threading.Thread(target=execute_command, args=("171.232.94.200:32883",))
     thread.start()
 
 read_output()
