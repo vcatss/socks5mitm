9bced57d8ba46da368100f49cb184b2c3dcd46ab
VC <namnguyen.vc@gmail.com>
2023-01-27 15:47:37 +0700

    zz

--- proxy.py
+++ proxy.py
@@ -126,13 +126,14 @@ def execute_command2():
             if output:
                 match = re.search(r'(\d+\.\d+\.\d+\.\d+):(\d+)', str(output.strip()))
                 client_ip = match.group(1)
+                print(f"{bcolors.OKBLUE}[*] {client_ip} {bcolors.WHITE}")
                 if match:
-                    if client_ip != "127.0.0.1" and  client_ip != "localhost" and client_ip != "0.0.0.0" and match.group(1) == allowip:
-                        print(f"{bcolors.OKGREEN}[*] Checked OK {bcolors.WHITE}")
-                        checked = True
-                    else:
+                    if client_ip != "127.0.0.1" and  client_ip != "localhost" and client_ip != "0.0.0.0" and match.group(1) != allowip:
                         print(f"{bcolors.FAIL}[*] Checked Fail {bcolors.WHITE}")
                         checked = False
+                    else:
+                        print(f"{bcolors.OKGREEN}[*] Checked OK {bcolors.WHITE}")
+                        checked = True
                     print(f"{bcolors.OKCYAN}[*] {match.group(1)}:{match.group(2)} Connection {bcolors.WHITE}")
                 print(output.strip())
         except Exception as e:
