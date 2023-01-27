a8db5708dcfa3bf86edd8d544d127f930cbf4e19
VC <namnguyen.vc@gmail.com>
2023-01-27 19:25:54 +0700

    zz

--- test.py
+++ test.py
@@ -8,12 +8,12 @@ output = subprocess.run(["ufw", "status", "numbered"], capture_output=True, text
 
 # Search for the rule with port 1080
 for line in output.stdout.split("\n"):
-    if port in line:
-        rule_number = line.split()[0]
-        rule_status = line.split()[1]
-        if rule_status == "allow":
-            # Delete the rule
-            subprocess.run(["ufw", "delete", rule_number])
-            print(f"Rule {rule_number} containing port {port} was deleted.")
-        else:
-            print(f"Rule {rule_number} containing port {port} is not 'allow'.")
+    rule_number = line.split()[0]
+    rule_status = line.split()[1]
+    print(line)
+    if rule_status == "allow":
+        # Delete the rule
+        subprocess.run(["ufw", "delete", rule_number])
+        print(f"Rule {rule_number} containing port {port} was deleted.")
+    else:
+        print(f"Rule {rule_number} containing port {port} is not 'allow'.")
