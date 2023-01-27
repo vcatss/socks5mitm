8f804c9a036c9460f011337ba6f2a046d2729dee
VC <namnguyen.vc@gmail.com>
2023-01-27 19:39:07 +0700

    zz

--- test.py
+++ test.py
@@ -9,5 +9,5 @@ output = subprocess.run(["ufw", "status", "numbered"], capture_output=True, text
 # Search for the rule with port 1080
 for line in output.stdout.split("\n"):
     numbers = re.findall(r'\d+', line)
-    if len(numbers) > 2:
+    if len(numbers) > 1:
         print(f"Rule number: {numbers[0]} : Port number: {numbers[1]}")
